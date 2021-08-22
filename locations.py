3e75ac4fb83e46ec048fe90e00e731cb06930c37
Sacha Moira Rinn <Moira.Rinn@gmail.com>
2021-08-22 18:08:25 -0400

    Mostly done, need to store checks in list and loop for display... Req functionality completed, site functional.

---
+++ locations.py
@@ -0,0 +1,6 @@
+
+def Locations():
+    location_options = [
+        "New York City, New York", "Los Angeles, California", "Chicago, Illinois", "Houston, Texas", "Phoenix, Arizona", "Philadelphia, Pennsylvania", "San Antonio, Texas", "San Diego, California", "Dallas, Texas", "Austin, Texas", "San Jose, California", "Fort Worth, Texas", "Jacksonville, Florida", "Columbus, Ohio", "Charlotte, North Carolina", "Indianapolis, Indiana", "San Francisco, California", "Seattle, Washington", "Denver, Colorado", "Washington, District of Columbia", "Boston,Massachusetts", "El Paso, Texas", "Nashville, Tennessee", "Oklahoma City, Oklahoma", "Las Vegas, Nevada", "Detroit, Michigan", "Portland, Oregon", "Memphis, Tennessee", "Louisville, Kentucky", "Milwaukee, Wisconsin", "Baltimore, Maryland", "Albuquerque, New Mexico", "Tucson, Arizona", "Mesa, Arizona", "Fresno, California", "Sacramento, California", "Atlanta, Georgia", "Kansas City, Missouri", "Colorado Springs, Colorado", "Raleigh, North Carolina", "Omaha, Nebraska", "Miami, Florida", "Long Beach, California", "Virginia Beach, Virginia", "Oakland, California", "Minneapolis, Minnesota", "Tampa, Florida", "Tulsa, Oklahoma", "Arlington, Texas", "Wichita, Kansas"
+    ]
+    return location_options
---
+++ server.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect, session
+from locations import Locations
+
+
+app = Flask(__name__)
+app.secret_key = 'WouldntYouLikeToKnow'
+
+Locations = Locations()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', locations=Locations)
+
+
+@app.route('/process', methods=['POST'])
+def sesData():
+    session['firstName'] = request.form['firstName']
+    session['lastName'] = request.form['lastName']
+    session['email'] = request.form['email']
+    session['location'] = request.form['location']
+    session['progLanguage'] = request.form['progLanguage']
+    session['satisfaction'] = request.form['satisfaction']
+    session['willReturn'] = request.form['willReturn']
+    session['goodThings'] = request.form['goodThings']
+    session['message'] = request.form['message']
+
+    return redirect('/submitted')
+
+
+@app.route('/submitted')
+def submitted():
+    return render_template('submitted.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
